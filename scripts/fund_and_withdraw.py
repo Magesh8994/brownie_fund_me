e9137507dc74357ce5ee1182e02cfc494bb1ddf5
Magesh8994 <mageshanand.dasarathan@gmail.com>
2021-12-30 08:40:49 +0530

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,24 @@
+from brownie import FundMe
+from brownie.network import account
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    enterance_fee = fund_me.getEntranceFee()
+    print(enterance_fee)
+    print(f"The current entry fees is {enterance_fee}")
+    print("Funding")
+    fund_me.fund({"from": account, "value": enterance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
+def main():
+    fund()
+    withdraw()
